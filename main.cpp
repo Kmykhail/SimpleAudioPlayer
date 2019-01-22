60e3482102cb767b7ce421882bb51266296a9e6c
kostiantyn.mykhailenko <kostiantyn.mykhailenko@zenuity.com>
2019-01-22 16:47:24 +0200

    a
    Signed-off-by: kostiantyn.mykhailenko <kostiantyn.mykhailenko@zenuity.com>

--- main.cpp
+++ main.cpp
@@ -2,6 +2,12 @@
 #include "MultimediaObj.hpp"
 
 int main() {
+    std::cout << "You can use mouse or key like:"
+                 "\n`s`-play"
+                 "\n`space`-stop"
+                 "\n`p`-pause"
+                 "\n`d`-next song"
+                 "\n`a`-previous song" << std::endl;
     MultimediaObj mptr;
     mptr.Init();
     return 0;
